4c9ed959aecb1d7d3ca27724ee8ff2ba3a527abd
Fernando <fer.tab@hotmail.com>
2022-08-26 20:03:45 -0300

    Update ejercicioActividad06.py

---
+++ ejercicioActividad06.py
@@ -0,0 +1,12 @@
+# Crear 2 variables enteras y mostrar la suma de ambas en pantalla
+
+num1 = 0
+num2 = 0
+
+# Código
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+print("La suma de los dos números es: ", num1 + num2)
+
+################################
+
