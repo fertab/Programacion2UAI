64fda65f8c4a114c3c2ad921fce8d3f1ef931015
Fernando <fer.tab@hotmail.com>
2022-08-26 20:08:19 -0300

    Update ejercicioActividad06.py

--- ejercicioActividad06.py
+++ ejercicioActividad06.py
@@ -10,3 +10,14 @@ print("La suma de los dos números es: ", num1 + num2)
 
 ################################
 
+# Dada una variable entera, sumarle 1 y mostrar el resultado en pantalla
+
+num = 0
+
+# Código
+num = int(input("Introduce un número: "))
+print("El número introducido es: ", num)
+print("El número introducido más 1 es: ", num + 1)
+
+################################
+
